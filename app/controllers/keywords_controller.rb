69c4bf74ebb7d24ca96887758d73443eb41ca692
Yuki Oyama <oja_yuki@mac.com>
2026-05-10 17:22:43 +0900

    お題投入

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,10 +1,11 @@
 class KeywordsController < ApplicationController
   def index
     @keywords = Keyword
-                  .includes(:kukais)
-                  .left_joins(:posts)
-                  .select("keywords.*, COUNT(posts.id) AS posts_count")
-                  .group("keywords.id")
-                  .order(:word)
+      .joins(:kukai_keywords)
+      .includes(:kukais)
+      .left_joins(:posts)
+      .select("keywords.*, COUNT(posts.id) AS posts_count")
+      .group("keywords.id")
+      .order(:word)
   end
 end
